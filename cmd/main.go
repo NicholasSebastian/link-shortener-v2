2e7b7e1f77086272d3118a3b7c90b943c6a25f8f
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2024-06-07 03:17:41 +0700

    wrote some boilerplate for the http server

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/NicholasSebastian/link-shortener-v2/internal/services"
 	"github.com/joho/godotenv"
 )
 
+const PORT = 8000
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalln("Server Error: Failed to load environment variables.")
+		log.Fatalln("failed to load environment variables")
+	}
+
+	mux := http.NewServeMux()
+	// TODO: Register the route handlers here.
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", PORT),
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("error running the HTTP server: %s\n", err)
 	}
 
 	username := ""
