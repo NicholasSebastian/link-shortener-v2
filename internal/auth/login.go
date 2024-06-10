22455ac6cedaa013b69241987b32b223f53460a3
Nicholas Sebastian <nicholashendrata110900@gmail.com>
2024-06-11 03:28:10 +0700

    implemented a bit of database logic

--- internal/auth/login.go
+++ internal/auth/login.go
@@ -1,18 +1,29 @@
 package auth
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/NicholasSebastian/link-shortener-v2/internal/services"
 )
 
+type credentials struct {
+	Username string
+	Password string
+}
+
 func HandleLogin(res http.ResponseWriter, req *http.Request) {
-	username := ""
-	password := "" // TODO: Retrieve these from the request body.
+	var c credentials
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&c); err != nil {
+		services.LogErrorf("Malformed Login Request Body: %s", err)
+	}
 
 	// TODO: Check if the given username and password are valid.
-	log.Println(username, password)
+	log.Println(c.Username, c.Password)
 
-	tokenstr := NewJwtToken(username)
+	tokenstr := NewJwtToken(c.Username)
 	log.Println(tokenstr)
 	// TODO: Store this in the user's cookies or payload or sth.
 
--- internal/models/users.go
+++ internal/models/users.go
@@ -1,1 +1,36 @@
 package models
+
+import (
+	"database/sql"
+)
+
+type Users struct {
+	database *sql.DB
+}
+
+func NewUsers(database *sql.DB) *Users {
+	return &Users{
+		database: database,
+	}
+}
+
+func (users *Users) Migrate() error {
+	query := `
+	CREATE TABLE IF NOT EXISTS users (
+		username TEXT PRIMARY KEY,
+		password TEXT NOT NULL
+	)`
+
+	_, err := users.database.Exec(query)
+	return err
+}
+
+func (users *Users) Create(username, password string) error {
+	_, err := users.database.Exec("INSERT INTO users (username, password) VALUES (?, ?)")
+	return err
+}
+
+func (users *Users) Exists(username, password string) (bool, error) {
+	// TODO: Check if a row with the given username and password exists.
+	return false, nil
+}
